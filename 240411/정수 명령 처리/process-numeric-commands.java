3d730eeb12cc8154857ab952a51bdc73aa81daca
soyun11 <146460450+soyun11@users.noreply.github.com>
2024-04-11 16:27:06 +0900

    Title: 정수 명령 처리, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240411/정수 명령 처리/process-numeric-commands.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        scanner.nextLine(); 
+        
+        for(int i = 0; i < a; i++) {
+            String input = scanner.nextLine();
+            String[] tokens = input.split(" ");
+            
+            if(tokens[0].equals("push")) {
+                int b = Integer.parseInt(tokens[1]);
+                stack.push(b);
+            }
+            if(tokens[0].equals("size")) {
+                System.out.println(stack.size());
+            }
+            if(tokens[0].equals("empty")) {
+                System.out.println(stack.isEmpty() ? 1 : 0);
+            }
+            if(tokens[0].equals("pop")) {
+                if(!stack.isEmpty()) {
+                    int b = stack.pop();
+                    System.out.println(b);
+                } else {
+                    System.out.println(-1);
+                }
+            }
+        }
+    }
+}
