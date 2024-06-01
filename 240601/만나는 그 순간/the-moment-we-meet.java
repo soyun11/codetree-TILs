de17fd591d04ee6587cab6ef0173815d5362d830
soyun11 <146460450+soyun11@users.noreply.github.com>
2024-06-01 17:59:30 +0900

    Title: 만나는 그 순간, Time: 223ms, Memory: 29MB, Status: Passed - CodeTree

--- 240601/만나는 그 순간/the-moment-we-meet.java
+++ 240601/만나는 그 순간/the-moment-we-meet.java
@@ -1,52 +1,54 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n=scanner.nextInt();
-        int m=scanner.nextInt();
-        int[]A=new int[100000];
-        A[0]=0; //A의 초기위치
-        int[]B=new int[100000];
-        B[0]=0;  //B의 초기위치
-        int Acurrent=0;//현재 A위치
-        int Aindex=0;
-        int Bcurrent=0;//현재 B위치
-        int Bindex=0;
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        
+        // 충분히 큰 배열로 설정
+        int[] A = new int[1000000];
+        int[] B = new int[1000000];
+        
+        A[0] = 0; // A의 초기 위치
+        B[0] = 0; // B의 초기 위치
+        
+        int Acurrent = 0; // 현재 A 위치
+        int Bcurrent = 0; // 현재 B 위치
+        int Aindex = 0; // A의 인덱스
+        int Bindex = 0; // B의 인덱스
 
-        for(int i=0;i<n;i++){
-            //A가 어떤 방향으로 몇 초 동안 이동했는지 나타내는 (d,t)
-            char dir=scanner.next().charAt(0);
-            int num=scanner.nextInt();
-            for(int j=0;j<num;j++){
+        // A의 이동 처리
+        for (int i = 0; i < n; i++) {
+            char direction = scanner.next().charAt(0);
+            int time = scanner.nextInt();
+            for (int j = 0; j < time; j++) {
                 Aindex++;
-                if(dir=='R'){
+                if (direction == 'R') {
                     Acurrent++;
-                }
-                else{//dir='L'
+                } else {
                     Acurrent--;
                 }
-                A[Aindex]=Acurrent;
+                A[Aindex] = Acurrent;
             }
-           
         }
-        for(int i=0;i<m;i++){
-            //A가 어떤 방향으로 몇 초 동안 이동했는지 나타내는 (d,t)
-            char dir=scanner.next().charAt(0);
-            int num=scanner.nextInt();
-            for(int j=0;j<num;j++){
+        
+        // B의 이동 처리
+        for (int i = 0; i < m; i++) {
+            char direction = scanner.next().charAt(0);
+            int time = scanner.nextInt();
+            for (int j = 0; j < time; j++) {
                 Bindex++;
-                if(dir=='R'){
+                if (direction == 'R') {
                     Bcurrent++;
-                }
-                else{//dir='L'
+                } else {
                     Bcurrent--;
                 }
-                B[Bindex]=Bcurrent;
+                B[Bindex] = Bcurrent;
             }
-           
         }
-// A와 B의 최초 만나는 시간 찾기
+        
+        // A와 B의 최초 만나는 시간 찾기
         int success = -1;
         int minLength = Math.min(Aindex, Bindex); // 두 배열의 최소 길이
         for (int i = 1; i <= minLength; i++) {
@@ -61,6 +63,5 @@ public class Main {
 
         // Scanner 닫기
         scanner.close();
-
     }
 }
