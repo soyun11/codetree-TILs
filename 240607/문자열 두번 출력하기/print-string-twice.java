a5ca736e51cf8717768548a0f3774f4e63a06712
soyun11 <146460450+soyun11@users.noreply.github.com>
2024-06-07 01:39:42 +0900

    Title: 문자열 두번 출력하기, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240607/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str=scanner.next();
+        System.out.println(str);
+        System.out.println(str);
+
+    }
+}
