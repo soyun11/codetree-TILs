00adbcb33444ad7ba014bedc5fae7302e29e25ff
soyun11 <146460450+soyun11@users.noreply.github.com>
2024-06-07 02:18:28 +0900

    Title: 문자열 범위 출력하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240607/문자열 범위 출력하기/print-string-in-range.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+
+        for(int i=2;i<10;i++){
+            System.out.print(str.charAt(i));
+        }
+    }
+}
