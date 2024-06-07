628ad1a46c0427e09f7bee1bdfe08aaef562b646
soyun11 <146460450+soyun11@users.noreply.github.com>
2024-06-07 20:43:31 +0900

    Title: 문자열에서 문자 출력, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240607/문자열에서 문자 출력/print-chars-from-word.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        for(int i=0;i<str.length();i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
