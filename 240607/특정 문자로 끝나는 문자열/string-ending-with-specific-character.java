82dc54760633681cbffb443a0ef5e9cff962c059
soyun11 <146460450+soyun11@users.noreply.github.com>
2024-06-07 20:31:45 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240607/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
+++ 240607/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
@@ -8,10 +8,15 @@ public class Main {
             arr[i]=sc.next();
         }
         char ch=sc.next().charAt(0);
+        int a=0;
         for(int i=0;i<10;i++){
             if(arr[i].charAt(arr[i].length()-1)==ch){
+                a++;
                 System.out.println(arr[i]);
             }
+        }
+        if(a==0){
+            System.out.println("None");
         }
     }
 }
