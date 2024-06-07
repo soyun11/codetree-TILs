eb2933c468af5aaebd0fed5fb77f432dd9a31b0d
soyun11 <146460450+soyun11@users.noreply.github.com>
2024-06-07 20:21:22 +0900

    Title: 단어로 구분하기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240607/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String[]arr=new String[10];
+        for(int i=0;i<10;i++){
+            arr[i]=sc.next();
+            System.out.println(arr[i]);
+        }
+    }
+}
